7a5bbc20eb088f6c0797089481210fd787b17357
Jeremy Leggat <jleggat@asu.edu>
2015-06-09 16:00:31 -0700

    Fix CRLF

--- header.php
+++ header.php
@@ -21,7 +21,6 @@
 <!--[if !(IE 7) | !(IE 8)  ]><!-->
 
 
-
 <!-- BEGIN html -->
 
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
@@ -29,38 +28,7 @@
 <!--<![endif]-->
 
 	<!-- BEGIN head -->
-
-
-<!-- Twitter Code for Reynolds Website -->
 	<head>
-<script src="//platform.twitter.com/oct.js" type="text/javascript"></script>
-<script type="text/javascript">
-twttr.conversion.trackPid('l5jw5');</script>
-<noscript>
-<img height="1" width="1" style="display:none;" alt="" src="https://analytics.twitter.com/i/adsct?txn_id=l5jw5&p_id=Twitter" />
-<img height="1" width="1" style="display:none;" alt="" src="//t.co/i/adsct?txn_id=l5jw5&p_id=Twitter" /></noscript>
-
-
-<!-- Facebook Conversion Code for From Facebook Ads to Reynolds Website -->
-<script>(function() {
-  var _fbq = window._fbq || (window._fbq = []);
-  if (!_fbq.loaded) {
-    var fbds = document.createElement('script');
-    fbds.async = true;
-    fbds.src = '//connect.facebook.net/en_US/fbds.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(fbds, s);
-    _fbq.loaded = true;
-  }
-})();
-window._fbq = window._fbq || [];
-window._fbq.push(['track', '6024373290416', {'value':'0.00','currency':'USD'}]);
-</script>
-<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6024373290416&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
-
-
-
-
 
 		<!-- Title -->
 
@@ -85,14 +53,12 @@ window._fbq.push(['track', '6024373290416', {'value':'0.00','currency':'USD'}]);
 		</title>
 
 
-
 		<!-- Meta Tags -->
 
 		<meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
-		
 
 		<!-- Favicon -->
 
@@ -111,7 +77,6 @@ window._fbq.push(['track', '6024373290416', {'value':'0.00','currency':'USD'}]);
 		<?php } ?>
 
 		
-
 		<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" title="<?php printf( __( '%s latest posts', THEME_NAME), esc_html( get_bloginfo('name'), 1 ) ); ?>" />
 
 		<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', THEME_NAME), esc_html( get_bloginfo('name'), 1 ) ); ?>" />
@@ -119,37 +84,15 @@ window._fbq.push(['track', '6024373290416', {'value':'0.00','currency':'USD'}]);
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 
-
 		<?php wp_head(); ?>	
-
 
 
 	<!-- END head -->
 
 	</head>
-
 	
 
 	<!-- BEGIN body -->
-    
-    <!-- Google Code for Remarketing Tag -->
-<!--------------------------------------------------
-Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
---------------------------------------------------->
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = 961762318;
-var google_custom_params = window.google_tag_params;
-var google_remarketing_only = true;
-/* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/961762318/?value=0&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
     
 
 	<body <?php body_class();?>>
