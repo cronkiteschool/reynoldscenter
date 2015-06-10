4f42ee6edfb9ca72c02064c0feabe5c887bb695e
Jeremy Leggat <jleggat@asu.edu>
2015-06-10 13:54:25 -0700

    remove function to stop heartbeat.
    This was not making a differnce with page load speed.

--- functions.php
+++ functions.php
@@ -4,17 +4,3 @@
  *
  * @package reynolds-center
  */
-
-/**
- * disable heartbeat on all but when editing pages or posts.
- * http://www.webhostinghub.com/help/learn/wordpress/heartbeat-admin-ajax-usage
- */
- 
-add_action( 'init', 'stop_heartbeat', 1 );
-
-function stop_heartbeat() {
-        global $pagenow;
-
-        if ( $pagenow != 'post.php' && $pagenow != 'post-new.php' )
-        wp_deregister_script('heartbeat');
-}
