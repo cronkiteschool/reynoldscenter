20112b380421a7917404609459e6fea4d42b3be1
Jeremy Leggat <jleggat@asu.edu>
2015-07-16 17:06:54 -0700

    Add function for facebook javascript
    Needed to print follow us button.

--- functions.php
+++ functions.php
@@ -1,6 +1,22 @@
 <?php
 /**
- * reynolds-center functions and definitions
+ * reynoldscenter functions and definitions
  *
- * @package reynolds-center
+ * @package reynoldscenter
  */
+
+function reynoldscenter_fb_inline_script() {
+?>
+<script>
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=105042694629";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+<?php
+}
+add_action( 'wp_footer', 'reynoldscenter_inline_script' );
+
