84d13e5c85106cbfde42796e4ea8a188b0dc60aa
Jeremy Leggat <jleggat@asu.edu>
2016-03-03 15:21:46 -0700

    Add Hotjar tracking code.
    Hotjar Tracking Code for http://businessjournalism.org/

--- header.php
+++ header.php
@@ -70,6 +70,17 @@
 
 		<?php wp_head(); ?>	
 
+		<!-- Hotjar Tracking Code for http://businessjournalism.org/ -->
+		<script>
+			(function(h,o,t,j,a,r){
+				h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+				h._hjSettings={hjid:162994,hjsv:5};
+				a=o.getElementsByTagName('head')[0];
+				r=o.createElement('script');r.async=1;
+				r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+				a.appendChild(r);
+			})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+		</script>
 
 	<!-- END head -->
 
